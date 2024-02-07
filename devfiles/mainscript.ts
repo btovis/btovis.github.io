b521523403ac108d9b58c20a5030cb1e665a3d33
CGDogan <126820728+CGDogan@users.noreply.github.com>
2024-02-07 21:04:05 +0000

    switch to vite

--- devfiles/mainscript.ts
+++
@@ -1,1 +0,0 @@
-document.body.innerHTML += "typescript script ran successfully. this text was added to html by a script"
--- script.js
+++
@@ -1,2 +0,0 @@
-document.body.innerHTML += "typescript script ran successfully. this text was added to html by a script";
-//# sourceMappingURL=script.js.map
