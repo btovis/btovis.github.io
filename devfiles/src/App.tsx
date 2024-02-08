106eb00e44025d59e6542b2c963f27142bfa93c5
CGDogan <126820728+CGDogan@users.noreply.github.com>
2024-02-08 11:54:32 +0000

    lpane

--- devfiles/src/App.tsx
+++ devfiles/src/App.tsx
@@ -9,29 +9,34 @@ function App() {
 
   return (
     <><div className="splitScreen">
-      <div className="panel">TOPPANE</div>
-      <div className="paner">RPANE</div>
-    </div>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      <div className="panel">LPANE</div>
+      <div className="paner">
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more.
         </p>
+
+
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more.
-      </p>
+    </div>
+
     </>
   )
 }
