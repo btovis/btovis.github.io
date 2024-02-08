de00020c8682376f952deab1771fc2fd7e5ac119
CGDogan <126820728+CGDogan@users.noreply.github.com>
2024-02-08 12:26:51 +0000

    separate app

--- devfiles/src/App.tsx
+++ devfiles/src/App.tsx
@@ -1,41 +1,28 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import Panel from './classes/Panel.js';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Panel from './classes/Panel.js'
+import Sidebar from './Sidebar.tsx'
+import MainPage from './MainPage.tsx'
+
 
 function App() {
     const [count, setCount] = useState(0);
 
-    return (
-        <>
-            <div className='splitScreen'>
-                <div className='panel'>LPANE</div>
-                <div className='paner'>
-                    <div>
-                        <a href='https://vitejs.dev' target='_blank'>
-                            <img src={viteLogo} className='logo' alt='Vite logo' />
-                        </a>
-                        <a href='https://react.dev' target='_blank'>
-                            <img src={reactLogo} className='logo react' alt='React logo' />
-                        </a>
-                    </div>
-                    <h1>Vite + React</h1>
-                    <div className='card'>
-                        <button onClick={() => setCount((count) => count + 1)}>
-                            count is {count}
-                        </button>
-                        <p>
-                            Edit <code>src/App.tsx</code> and save to test HMR
-                        </p>
-                    </div>
-                    <p className='read-the-docs'>
-                        Click on the Vite and React logos to learn more.
-                    </p>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <><div className="splitScreen">
+      <div className="panel"><Sidebar></Sidebar></div>
+      <div className="paner">
+        <MainPage></MainPage>
+
+
+
+      </div>
+    </div>
+
+    </>
+  )
 }
 
 export default App;
---
+++ devfiles/src/Sidebar.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Panel from './classes/Panel.js'
+
+function Sidebar() {
+
+  return (
+   <>
+      LPANE
+    </>
+  )
+}
+
+export default Sidebar
