99a6527552b2120a887c14dc016ac58df10d9a5c
Hex27 <leothepro555@gmail.com>
2024-02-13 22:17:08 +0000

    Added refresh methods
    As well as a test widget add button in panel. Also re-added setup.ts

---
+++ devfiles/src/ui/PanelComp.test.jsx
@@ -0,0 +1,13 @@
+// import { test, expect } from 'vitest';
+// import { render } from '@testing-library/react';
+// import { React } from 'react';
+// import PageManager from '../classes/PageManager.ts';
+// import PanelComp from '../ui/PanelComp';
+
+// test('renders without crashing', () => {
+//     var manager = new PageManager();
+//     const { getByText } = render(<PanelComp panelIdx={0} pageManager={manager} />);
+//     const linkElement = getByText(/Vite \+ React/i);
+//     expect(linkElement).toBeInTheDocument();
+// });
+//Don't test anything with useState in it. React really doesn't like it.
--- devfiles/src/ui/SidebarComp.tsx
+++ devfiles/src/ui/SidebarComp.tsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 import '../App.css';
 import PageManager from '../classes/PageManager.js';
 
 function SidebarComp(params: { pageManager: PageManager }) {
+    //State machine mechanism. Have this arbitrary integer for a makeshift refresh
+    const [r, dud] = useState(0);
+    const refresh = () => dud(r + 1);
     return <>LPANE</>;
 }
 
