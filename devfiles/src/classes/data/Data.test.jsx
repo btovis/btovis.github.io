c3f332bff301eb790d1f6d209b42534531e39162
George Ogden <george-ogden@outlook.com>
2024-02-14 18:51:36 +0000

    write test for leading CSV data

--- devfiles/src/classes/data/Data.test.jsx
+++ devfiles/src/classes/data/Data.test.jsx
@@ -1,5 +1,23 @@
 import { test, expect } from 'vitest';
+import { Data } from './Data';
+import fs from 'fs';
+import { Buffer } from 'buffer';
 
-test('placeholder', () => {
-    expect(1).toBe(1);
+test('load CSV and data added', async () => {
+    const data = new Data();
+    const filename = 'devfiles/src/tests/testdata.csv';
+
+    const fd = await fs.promises.open(filename, 'r');
+    const buffer = Buffer.alloc(1);
+
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+        const { bytesRead } = await fd.read(buffer, 0, 1, null);
+        if (bytesRead === 0) break;
+    }
+
+    expect(data.readDatabase()).toStrictEqual([]);
+    data.addCSV(filename, buffer);
+    expect(data.readDatabase()).toStrictEqual([]);
+    await fd.close();
 });
