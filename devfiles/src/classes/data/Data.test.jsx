e9449e21095f7d2bae59e1135f81d745f9a322e3
George Ogden <george-ogden@outlook.com>
2024-02-14 19:03:32 +0000

    refactor CSV test to use a fixture

--- devfiles/src/classes/data/Data.test.jsx
+++ devfiles/src/classes/data/Data.test.jsx
@@ -3,21 +3,28 @@ import { Data } from './Data';
 import fs from 'fs';
 import { Buffer } from 'buffer';
 
-test('load CSV and data added', async () => {
-    const data = new Data();
-    const filename = 'devfiles/src/tests/testdata.csv';
+const DataTest = test.extend({
+    data: async ({ task }, use) => {
+        const data = new Data();
+        const filename = 'devfiles/src/tests/testdata.csv';
 
-    const fd = await fs.promises.open(filename, 'r');
-    const buffer = Buffer.alloc(1);
+        const fd = await fs.promises.open(filename, 'r');
+        const buffer = Buffer.alloc(1);
+        var byteArray = new Uint8Array(0);
 
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-        const { bytesRead } = await fd.read(buffer, 0, 1, null);
-        if (bytesRead === 0) break;
+        // eslint-disable-next-line no-constant-condition
+        while (true) {
+            const { bytesRead } = await fd.read(buffer, 0, 1, null);
+            byteArray = new Uint8Array([...byteArray, ...buffer]);
+            if (bytesRead === 0) break;
+        }
+
+        expect(data.readDatabase()).toStrictEqual([]);
+        data.addCSV(filename, byteArray);
+        await use(data);
     }
+});
 
-    expect(data.readDatabase()).toStrictEqual([]);
-    data.addCSV(filename, buffer);
-    expect(data.readDatabase()).toStrictEqual([]);
-    await fd.close();
+DataTest('load CSV and test for data added', async ({ data }) => {
+    expect(data.readDatabase()).not.toStrictEqual([]);
 });
