10f0f8a51c83e82a34225c8f2a9f11216922aa53
George Ogden <38294960+George-Ogden@users.noreply.github.com>
2024-02-15 10:40:49 +0000

    Integrate tests into main branch (#31)
    * upload sample test file
    
    * write test for leading CSV data
    
    * refactor CSV test to use a fixture
    
    * improve test performance
    
    read buffer in chunks to reduce load time
    
    * add breaking test
    
    column accessor code is incorrect
    
    * fix getColumnIndex method
    
    fix OBOE
    modify Attribute enum
    
    * parametrise test for different values in CSV
    
    * test PageManager constructor
    
    * test PageManager constructor
    
    * run tests during testing

--- devfiles/src/classes/data/Data.test.jsx
+++ devfiles/src/classes/data/Data.test.jsx
@@ -1,5 +1,71 @@
 import { test, expect } from 'vitest';
+import { Attribute, Data } from './Data';
+import fs from 'fs';
+import { Buffer } from 'buffer';
 
-test('placeholder', () => {
-    expect(1).toBe(1);
+const filename = 'devfiles/src/tests/testdata.csv';
+
+async function loadData() {
+    const data = new Data();
+
+    const fd = await fs.promises.open(filename, 'r');
+    const bufferSize = 1024;
+    const buffer = Buffer.alloc(bufferSize);
+    var byteArray = new Uint8Array(0);
+
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+        const { bytesRead } = await fd.read(buffer, 0, bufferSize, null);
+        const chunk = buffer.slice(0, bytesRead);
+        byteArray = new Uint8Array([...byteArray, ...chunk]);
+        if (bytesRead === 0) break;
+    }
+
+    expect(data.readDatabase()).toStrictEqual([]);
+    data.addCSV(filename, byteArray);
+    return data;
+}
+
+const DataTest = test.extend({
+    data: async ({ task }, use) => {
+        const data = await loadData();
+        await use(data);
+    }
 });
+
+DataTest('load CSV and test for data added', async ({ data }) => {
+    expect(data.readDatabase()).not.toStrictEqual([]);
+});
+
+DataTest.each([
+    { index: 0, attribute: Attribute.latitude, expected: 56.915 },
+    { index: 3, attribute: Attribute.longitude, expected: -4.14185 },
+    { index: 2, attribute: Attribute.speciesLatinName, expected: 'Troglodytes troglodytes' },
+    { index: 8, attribute: Attribute.projectName, expected: 'Acoustic Classifier Development' },
+    { index: 0, attribute: Attribute.species, expected: 46 },
+    { index: 5, attribute: Attribute.fileName, expected: filename }
+])(
+    'get accessor for columns with index $index and attribute $attribute',
+    async ({ index, attribute, expected }) => {
+        const data = await loadData();
+        const accessor = data.getAccessorForColumn(attribute);
+        expect(accessor).not.toBeNull();
+        const dataValue = accessor(data.readDatabase()[index]);
+        if (isNaN(dataValue) && isNaN(expected)) {
+            console.log('dataValue: ' + typeof dataValue);
+            switch (typeof dataValue) {
+                case 'string':
+                    // Check if expected is a string
+                    expect(dataValue).toBe(expected);
+                    break;
+                default:
+                    // Check if expected is a setItem (filename)
+                    expect(dataValue.value).toBe(expected);
+                    break;
+            }
+        } else {
+            // Check if expected is a number
+            expect(dataValue).toBeCloseTo(expected);
+        }
+    }
+);
