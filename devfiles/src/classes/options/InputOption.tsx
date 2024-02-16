672d7fd764839bbf27a5daa8899a443ecfd53c99
George Ogden <george-ogden@outlook.com>
2024-02-16 17:24:03 +0000

    change panel constructor to use static fromPanel method

--- devfiles/src/classes/options/InputOption.tsx
+++ devfiles/src/classes/options/InputOption.tsx
@@ -3,12 +3,13 @@ import Query from '../query/Query';
 
 export default abstract class InputOption {
     public readonly name: string;
-    protected readonly panel: Panel;
-    public constructor(panel: Panel, name: string) {
+    public constructor(name: string) {
         this.name = name;
-        this.panel = panel;
     }
     public abstract query(): Query;
     public abstract render(): JSX.Element[];
     public abstract callback(newValue: any);
+    public static fromPanel(panel: Panel): InputOption {
+        throw new Error('Not implemented, please call fromPanel on a subclass of InputOption');
+    }
 }
