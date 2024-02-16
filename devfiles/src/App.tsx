93a8e1216482e9506f3a1730d53c7266e7f306e6
Hex27 <leothepro555@gmail.com>
2024-02-16 17:33:18 +0000

    Added user feedback to the drag and drop process. Additionally, multiple files can now be dragged in at a time.

--- devfiles/src/App.tsx
+++ devfiles/src/App.tsx
@@ -1,69 +1,109 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SidebarComp from './ui/SidebarComp.tsx';
 import MainPage from './ui/MainPage.tsx';
 import PageManager from './classes/PageManager.ts';
+import { Fade, Spinner } from 'react-bootstrap';
 
 function App() {
+    const [overlayVisible, setOverlayVisible] = useState(false);
+    const [overlayMessage, setOverlayMessage] = useState('');
+    const [isOverlaySuccess, setIsOverlaySuccess] = useState(true);
     const [r, dud] = useState(0);
     const [pageManager, _] = useState(new PageManager());
     pageManager.refreshEverything = () => dud(r + 1);
 
     const borderRef = useRef(null);
+    const spinnerRef = useRef(null);
     return (
         <div
             onDragOver={(event) => {
                 event.preventDefault();
-                if (borderRef.current) borderRef.current.style.opacity = '80%';
+                if (borderRef.current) borderRef.current.style.opacity = 0.8;
             }}
-            onDragExit={(event) => {
+            onDragLeave={(event) => {
                 event.preventDefault();
-                if (borderRef.current) borderRef.current.style.opacity = '0%';
+                if (borderRef.current) borderRef.current.style.opacity = 0;
             }}
             onDrop={(event) => {
                 event.stopPropagation();
                 event.preventDefault();
-                if (borderRef.current) borderRef.current.style.opacity = '0%';
+                if (spinnerRef.current) spinnerRef.current.style.opacity = 1;
 
+                const rejected = [];
                 (async () => {
-                    const file = event.dataTransfer.files[0];
-                    if (!file.name.toLowerCase().endsWith('csv')) {
-                        alert('Only CSV files from the BTO pipeline are allowed!');
-                        return;
-                    }
-                    window['pageManager'] = pageManager;
-                    console.log(event.dataTransfer.files[0]);
-                    pageManager
-                        .getData()
-                        .addCSV(file.name, new Uint8Array(await file.arrayBuffer()));
+                    await Array.prototype.forEach.call(event.dataTransfer.files, async (file) => {
+                        if (!file.name.toLowerCase().endsWith('csv')) {
+                            rejected.push(file.name);
+                            return;
+                        }
+                        window['pageManager'] = pageManager;
+                        console.log(file);
+                        pageManager
+                            .getData()
+                            .addCSV(file.name, new Uint8Array(await file.arrayBuffer()));
+                    });
                 })().then(() => {
-                    //Do feedback here to say that it was uploaded
-                    console.log('Read file.');
+                    if (borderRef.current) borderRef.current.style.opacity = 0;
+                    if (spinnerRef.current) spinnerRef.current.style.opacity = 0;
+                    setIsOverlaySuccess(rejected.length == 0);
+                    if (rejected.length > 0)
+                        setOverlayMessage(
+                            'Did not parse:\n - ' +
+                                rejected.join('\n - ') +
+                                '\n Only CSVs exported from the BTO pipeline can be processed.'
+                        );
+                    else setOverlayMessage('Loaded file(s).');
+                    setOverlayVisible(true);
                 });
             }}
         >
-            {/*Move this to App.css later*/}
-            <div
-                ref={borderRef}
-                id='border'
-                style={{
-                    position: 'absolute',
-                    width: '98%',
-                    height: '98%',
-                    left: '1%',
-                    top: '1%',
-                    borderStyle: 'dotted',
-                    pointerEvents: 'none',
-                    backgroundColor: '#eeeeee',
-                    opacity: '0%'
-                }}
-                className='rounded'
-            >
-                <p>
-                    <strong>Upload File</strong>
-                </p>
+            {/* The stuff above is solely for the drag and drop handling. */}
+            {/* fileUploadIndic is what shows up when you drag a file in */}
+            <div ref={borderRef} className='fileUploadIndic rounded'>
+                <div>
+                    <p>
+                        <strong>Upload File</strong>
+                    </p>
+                    <Spinner
+                        ref={spinnerRef}
+                        style={{ opacity: 0 }}
+                        animation='border'
+                        role='status'
+                    >
+                        <span className='visually-hidden'>Loading...</span>
+                    </Spinner>
+                </div>
             </div>
+            {/* This is the red or green banner that displays feedback*/}
+            <Fade
+                appear={true}
+                timeout={1000}
+                onEntered={() =>
+                    setTimeout(() => setOverlayVisible(false), isOverlaySuccess ? 500 : 2000)
+                }
+                in={overlayVisible}
+            >
+                <div
+                    className={
+                        (isOverlaySuccess ? 'fileUploadSuccess' : 'fileUploadWarn') + ' rounded'
+                    }
+                >
+                    <table>
+                        <tbody>
+                            {overlayMessage.split('\n').map((line, lineIdx) => (
+                                <tr key={lineIdx}>
+                                    <td>
+                                        <strong>{line}</strong>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </Fade>
+            {/* This leads to the rest of the UI components*/}
             <div className='splitScreen'>
                 <div className='sidebar'>
                     <SidebarComp pageManager={pageManager}></SidebarComp>
