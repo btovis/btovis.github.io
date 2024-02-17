a6235db37db5ec1e6f555516030562db6a325caf
CGDogan <126820728+CGDogan@users.noreply.github.com>
2024-02-17 19:20:59 +0000

    Quick build fix for Global options (George)
    For Global options can pass null to panel

--- devfiles/src/classes/options/InputOption.tsx
+++ devfiles/src/classes/options/InputOption.tsx
@@ -3,13 +3,13 @@ import Query from '../query/Query';
 
 export default abstract class InputOption {
     public readonly name: string;
-    public constructor(name: string) {
+    public constructor(panel: Panel, name: string) {
+        if (panel)
+            // @ts-expect-error: how to allow for both panel and global? currently pass null
+            this.panel = panel;
         this.name = name;
     }
     public abstract query(): Query;
     public abstract render(): JSX.Element[];
     public abstract callback(newValue: any);
-    public static fromPanel(panel: Panel): InputOption {
-        throw new Error('Not implemented, please call fromPanel on a subclass of InputOption');
-    }
 }
