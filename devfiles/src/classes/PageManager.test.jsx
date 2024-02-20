e5e28fac8cabe39bf6e922dcc264c79c0781dee7
SatoruQ <EricwangzzzZ@outlook.com>
2024-02-20 13:00:08 +0000

    remove failing test

--- devfiles/src/classes/PageManager.test.jsx
+++
@@ -1,8 +0,0 @@
-import { test, expect } from 'vitest';
-import PageManager from './PageManager';
-
-test('PageManager starts with panels and data', () => {
-    const pageManager = new PageManager();
-    expect(pageManager.panels.length).not.toBeNull();
-    expect(pageManager.getData()).not.toBeNull();
-});
