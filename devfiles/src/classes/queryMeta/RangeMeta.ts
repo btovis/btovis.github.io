6332c1d2e8f753e0ecb588ab8f176c26fbdb1c51
CGDogan <126820728+CGDogan@users.noreply.github.com>
2024-02-20 20:25:59 +0000

    *forgot this

--- devfiles/src/classes/queryMeta/RangeMeta.ts
+++ devfiles/src/classes/queryMeta/RangeMeta.ts
@@ -1,6 +1,6 @@
 export default class RangeMeta {
     // -Infinity, +Infinity, undefined when no data loaded or data doesn't have time
-    value: [low: number | Date, up: number | Date, colI: number | undefined];
+    public value: [low: number | Date, up: number | Date, colI: number | undefined];
     public constructor(arr) {
         this.value = arr;
     }
