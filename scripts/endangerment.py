163574de938c98208b55ee84c1e1a8b63d7959f2
George Ogden <38294960+George-Ogden@users.noreply.github.com>
2024-02-20 12:30:27 +0000

    Endangered Species (#65)
    * add script to download species data
    
    * run with multiple lists of species
    
    * upload lists of species
    
    * upload species data to assets
    
    * rename files
    
    use endangerment rather than species as term
    
    * create utilities for getting vulnerability of species
    
    * add test for species vulnerability status

---
+++ scripts/endangerment.py
@@ -0,0 +1,62 @@
+import json
+from glob import glob
+import os
+import time
+from tqdm import tqdm
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+# Load species names from file
+species_names = []
+for filename in glob("scripts/species-lists/*species.txt"):
+    with open(filename) as f:
+        species_names += f.read().splitlines()
+
+# Initialize WebDriver
+driver = webdriver.Chrome()
+driver.get("https://www.iucnredlist.org/")
+
+# Load existing species data if available
+species = {}
+species_file_path = "./devfiles/src/assets/endangerment.json"
+if os.path.exists(species_file_path):
+    with open(species_file_path) as f:
+        species = json.load(f)
+
+# Function to update species data
+def update_species(bird_name, category):
+    species[bird_name] = category
+
+# Scrape data for animals not already in the endangerment data
+for species_name in tqdm(species_names, desc="Scraping endangerment data"):
+    species_name = species_name.strip().lower()
+    if species_name in species:
+        continue
+    try:
+        # Search for the species on the IUCN Red List website
+        search_input = driver.find_element(By.CSS_SELECTOR, "input[type='search'].search.search--site")
+        search_input.clear()
+        search_input.send_keys(species_name + Keys.RETURN)
+        time.sleep(2)
+        
+        # Extract category information if found
+        result = driver.find_element(By.CSS_SELECTOR, ".species-category.-icon")
+        classes = result.get_attribute("class")
+        for c in classes.split():
+            if c.startswith("species-category--"):
+                update_species(species_name, c.split("--")[1])
+                break
+        else:
+            update_species(species_name, "unknown")
+    except NoSuchElementException as e:
+        update_species(species_name, "unknown")
+    
+    # Save updated species data
+    with open(species_file_path, "w") as f:
+        json.dump(species, f)
+
+# Close the WebDriver
+driver.quit()
