552abf1b84d7c7d5a0fa74d5a33953407b4c95f6
Kai Sun <71438287+cshuenss@users.noreply.github.com>
2024-02-22 15:09:52 +0000

    render time selectors
    no functionality yet

--- devfiles/src/classes/options/TimeRange.tsx
+++ devfiles/src/classes/options/TimeRange.tsx
@@ -1,12 +1,38 @@
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateTimePicker, dateTimePickerTabsClasses } from '@mui/x-date-pickers/DateTimePicker';
 import { Query } from '../query/Query';
 import InputOption from './InputOption';
 
 export default class TimeRange extends InputOption {
+    private fromDate: Date;
+    private toDate: Date;
+
     public render(): JSX.Element[] {
-        return [<p>Time Range: {this.name}</p>];
+        return [
+            <div>
+                <p id='elephant'>elephant</p>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DateTimePicker
+                        label='From'
+                        onChange={(value) =>
+                            this.callback({
+                                which: 0,
+                                datetime: value
+                            })
+                        }
+                    />
+                    <DateTimePicker label='To' />
+                </LocalizationProvider>
+            </div>
+        ];
     }
     public callback(newValue: any): void {
-        throw new Error('Method not implemented.');
+        if (newValue.which === 0) {
+            this.fromDate = newValue.datetime;
+        } else {
+            this.toDate = newValue.datetime;
+        }
     }
     public query(): Query {
         throw new Error('Method not implemented.');
