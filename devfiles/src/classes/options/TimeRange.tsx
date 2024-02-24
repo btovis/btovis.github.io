addcfdbd2c374bc25d46a8478e3ff7e6a1e95de9
cshuenss <71438287+cshuenss@users.noreply.github.com>
2024-02-24 13:47:53 +0000

    implemented timerange frontend (#117)
    * render time selectors
    
    no functionality yet
    
    * fromdate todate (crashes)
    
    * timerange frontend
    
    implemented frontend functionality for timerange
    
    * Changed DateTimeField to DatePicker
    
    Also unify title style with bold
    
    ---------
    
    Co-authored-by: Hex27 <leothepro555@gmail.com>

--- devfiles/src/classes/options/TimeRange.tsx
+++ devfiles/src/classes/options/TimeRange.tsx
@@ -1,12 +1,69 @@
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import dayjs, { Dayjs } from 'dayjs';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import Panel from '../Panel';
 import { Query } from '../query/Query';
 import InputOption from './InputOption';
 
 export default class TimeRange extends InputOption {
+    private minDate: Dayjs;
+    private maxDate: Dayjs;
+    public fromDate: Dayjs;
+    public toDate: Dayjs;
+
+    public constructor(panel: Panel, name: string, minDate: Date, maxDate: Date) {
+        super(panel, name);
+        this.minDate = dayjs(minDate);
+        this.maxDate = dayjs(maxDate);
+        this.fromDate = this.minDate;
+        this.toDate = this.maxDate;
+    }
+
     public render(): JSX.Element[] {
-        return [<p>Time Range: {this.name}</p>];
+        return [
+            <div className='sidebar-padding'>
+                <p>
+                    <strong>Filter by time</strong>
+                </p>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                        label='From'
+                        format='YYYY/MM/DD h:mm A'
+                        defaultValue={this.minDate}
+                        minDate={this.minDate}
+                        maxDate={this.toDate}
+                        onChange={(value) =>
+                            this.callback({
+                                which: 0,
+                                datetime: value
+                            })
+                        }
+                    />
+                    <p></p>
+                    <DatePicker
+                        label='To'
+                        format='YYYY/MM/DD h:mm A'
+                        defaultValue={this.maxDate}
+                        minDate={this.fromDate}
+                        maxDate={this.maxDate}
+                        onChange={(value) =>
+                            this.callback({
+                                which: 1,
+                                datetime: value
+                            })
+                        }
+                    />
+                    <p className='text-warning' id='warning-if-time-range-zero'></p>
+                </LocalizationProvider>
+            </div>
+        ];
     }
     public callback(newValue: any): void {
-        throw new Error('Method not implemented.');
+        if (newValue.which === 0) {
+            this.fromDate = newValue.datetime;
+        } else {
+            this.toDate = newValue.datetime;
+        }
     }
     public query(): Query {
         throw new Error('Method not implemented.');
