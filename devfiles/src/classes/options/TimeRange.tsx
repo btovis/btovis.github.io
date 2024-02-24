21a99d64a521ff1c8774eb51f5933a7aa80e041b
Kai Sun <71438287+cshuenss@users.noreply.github.com>
2024-02-24 10:59:53 +0000

    fromdate todate (crashes)

--- devfiles/src/classes/options/TimeRange.tsx
+++ devfiles/src/classes/options/TimeRange.tsx
@@ -1,12 +1,21 @@
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateTimePicker, dateTimePickerTabsClasses } from '@mui/x-date-pickers/DateTimePicker';
+import Panel from '../Panel';
 import { Query } from '../query/Query';
 import InputOption from './InputOption';
 
 export default class TimeRange extends InputOption {
-    private fromDate: Date;
-    private toDate: Date;
+    private minDate: Date;
+    private maxDate: Date;
+    public fromDate: Date;
+    public toDate: Date;
+
+    public constructor(panel: Panel, name: string, minDate: Date, maxDate: Date) {
+        super(panel, name);
+        this.minDate = minDate;
+        this.maxDate = maxDate;
+    }
 
     public render(): JSX.Element[] {
         return [
@@ -15,6 +24,7 @@ export default class TimeRange extends InputOption {
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
                     <DateTimePicker
                         label='From'
+                        minDateTime={this.minDate}
                         onChange={(value) =>
                             this.callback({
                                 which: 0,
