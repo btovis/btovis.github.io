71a0996fbadb486c0b029adf68ed58160c033978
CGDogan <126820728+CGDogan@users.noreply.github.com>
2024-02-24 20:08:09 +0000

    Small updates (#121)
    * small data fixes
    
    - Remove date objects
    - sort columns
    - use default sort
    
    * Faster adding multiple CSVs
    
    * Warn on outdated browser
    
    * RangeMeta empty when data not has col

--- devfiles/src/App.tsx
+++ devfiles/src/App.tsx
@@ -6,6 +6,13 @@ import MainPage from './ui/MainPage.tsx';
 import PageManager from './classes/PageManager.ts';
 import { Fade, Spinner } from 'react-bootstrap';
 
+// https://caniuse.com/?search=es2020 "Feature support list"
+// We target ES2020, 95% of browsers
+if (!Promise.allSettled) {
+    document.body.innerHTML =
+        "BTO Pipeline CSV visualization tool has detected that you're using an outdated browser. Unfortunately this browser is not supported. Please upgrade to a newer browser. Thank you.";
+}
+
 function App() {
     const [overlayVisible, setOverlayVisible] = useState(false);
     const [overlayMessage, setOverlayMessage] = useState('');
@@ -40,12 +47,13 @@ function App() {
                 }
                 window['pageManager'] = pageManager;
                 try {
-                    pageManager.addCSV(file.value.name, file.value.data);
+                    pageManager.addCSV(file.value.name, file.value.data, true);
                 } catch (e) {
                     rejected.push(file.value.name + ': ' + e);
                     return;
                 }
             });
+            pageManager.finaliseAddingCSVs();
             if (borderRef.current) borderRef.current.style.opacity = 0;
             if (spinnerRef.current) spinnerRef.current.style.opacity = 0;
             setIsOverlaySuccess(rejected.length == 0);
--- devfiles/src/classes/widgets/TableWidget.tsx
+++ devfiles/src/classes/widgets/TableWidget.tsx
@@ -77,7 +77,7 @@ export default class TableWidget extends Widget {
         if (arrLength == 0) return [];
 
         // Order the strings
-        arr.sort((a, b) => (a == b ? 0 : a < b ? -1 : 1));
+        arr.sort();
 
         // Collect same strings, they're consecutive
         //Potential optimisation to build the DOM straight in here
@@ -111,6 +111,8 @@ export default class TableWidget extends Widget {
                 /* Data doesn't have that - skip */
             }
         });
+
+        this.columns.sort((a, b) => a[1] - b[1]);
 
         const data = this.panel.dataFilterer.getData()[0];
         const dataLength = this.panel.dataFilterer.getData()[1];
