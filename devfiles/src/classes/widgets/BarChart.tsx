72baab6ee10aebe8e8bfe460c65312346f33569f
George Ogden <george-ogden@outlook.com>
2024-02-27 15:48:17 +0000

    draw bar chart with correct data

--- devfiles/src/classes/widgets/BarChart.tsx
+++ devfiles/src/classes/widgets/BarChart.tsx
@@ -2,6 +2,7 @@ import Widget from './Widget.js';
 import WidgetConfig from './WidgetConfig.js';
 import { Data, Attribute } from '../data/Data.js';
 import Row from '../data/Row.js';
+import { MonthGrouping } from './Grouping.js';
 import Sidebar from '../Sidebar.js';
 import ExportFileType from './ExportFileType.js';
 import Plot from 'react-plotly.js';
@@ -11,54 +12,7 @@ export default class BarChart extends Widget {
         return new Sidebar([]);
     }
     public render(): JSX.Element {
-        const grouping = 'month';
-        const months = [
-            'January',
-            'February',
-            'March',
-            'April',
-            'May',
-            'June',
-            'July',
-            'August',
-            'September',
-            'October',
-            'November',
-            'December'
-        ];
-        const timeColIdx = this.panel.dataFilterer.getColumnIndex(Attribute.time);
-        const dateColIdx = this.panel.dataFilterer.getColumnIndex(Attribute.actualDate);
-        const [data, length]: [any[][], number] = this.panel.dataFilterer.getData();
-        const dataSubset: any[][] = data.slice(0, length);
-        const groupIndices = Array(12)
-            .fill(0)
-            .map(() => []);
-        for (let i = 0; i < dataSubset.length; i++) {
-            const time = dataSubset[i][timeColIdx];
-            const date = dataSubset[i][dateColIdx];
-            const datetime = new Date(date + ' ' + time);
-            const group = datetime.getMonth();
-            groupIndices[group].push(i);
-        }
-        const speciesMeta = this.panel.dataFilterer.getDataStats().getSpeciesMeta();
-        const traces = speciesMeta.speciesList().map((species, idx) => {
-            const speciesColIdx = this.panel.dataFilterer.getColumnIndex(
-                Attribute.speciesLatinName
-            );
-            const counts = groupIndices.map((indices) =>
-                indices.reduce((a, b) => (a + (dataSubset[b][speciesColIdx] == species) ? 1 : 0), 0)
-            );
-            return {
-                type: 'bar',
-                x: months,
-                y: counts,
-                marker: {
-                    color: Array(12).fill(this.config.traceColor[idx])
-                },
-                name: speciesMeta.formattedName(species)
-            };
-        });
-        const plotData = traces;
+        const grouping = new MonthGrouping(this.panel.dataFilterer);
         const plotLayout = {
             width: 400,
             height: 210,
@@ -73,11 +27,17 @@ export default class BarChart extends Widget {
                 t: 65
             }
         };
+        const { traces, layout } = grouping.getChart(
+            {
+                type: 'bar'
+            },
+            plotLayout
+        );
         const plotConfig = {
             //staticPlot: true,
             modeBarButtonsToRemove: ['zoomIn2d', 'zoomOut2d']
         };
-        return <Plot data={plotData} layout={plotLayout} config={plotConfig} />;
+        return <Plot data={traces} layout={layout} config={plotConfig} />;
     }
     public delete(): void {
         //throw new Error('Method not implemented.');
