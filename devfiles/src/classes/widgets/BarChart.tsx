a3d73749ee1354fbe2717705c7649e6d03bac94f
George Ogden <george-ogden@outlook.com>
2024-02-27 11:29:41 +0000

    display real data on bar chart

--- devfiles/src/classes/widgets/BarChart.tsx
+++ devfiles/src/classes/widgets/BarChart.tsx
@@ -1,6 +1,7 @@
 import Widget from './Widget.js';
 import WidgetConfig from './WidgetConfig.js';
-import { Data } from '../data/Data.js';
+import { Data, Attribute } from '../data/Data.js';
+import Row from '../data/Row.js';
 import Sidebar from '../Sidebar.js';
 import ExportFileType from './ExportFileType.js';
 import Plot from 'react-plotly.js';
@@ -10,25 +11,56 @@ export default class BarChart extends Widget {
         return new Sidebar([]);
     }
     public render(): JSX.Element {
-        const trace1 = {
-            type: 'bar',
-            x: [1, 2, 3],
-            y: [2, 5, 3],
-            marker: {
-                color: Array(3).fill(this.config.traceColor[0])
-            }
-        };
-        const trace2 = {
-            type: 'bar',
-            x: [1, 2, 3],
-            y: [4, 2, 6],
-            marker: {
-                color: Array(3).fill(this.config.traceColor[1])
-            }
-        };
-        const plotData = [trace1, trace2];
+        const grouping = 'month';
+        const months = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December'
+        ];
+        const timeColIdx = this.panel.dataFilterer.getColumnIndex(Attribute.time);
+        const dateColIdx = this.panel.dataFilterer.getColumnIndex(Attribute.actualDate);
+        const [data, length]: [any[][], number] = this.panel.dataFilterer.getData();
+        const dataSubset: any[][] = data.slice(0, length);
+        const groupIndices = Array(12)
+            .fill(0)
+            .map(() => []);
+        for (let i = 0; i < dataSubset.length; i++) {
+            const time = dataSubset[i][timeColIdx];
+            const date = dataSubset[i][dateColIdx];
+            const datetime = new Date(date + ' ' + time);
+            const group = datetime.getMonth();
+            groupIndices[group].push(i);
+        }
+        const speciesMeta = this.panel.dataFilterer.getDataStats().getSpeciesMeta();
+        const traces = speciesMeta.speciesList().map((species, idx) => {
+            const speciesColIdx = this.panel.dataFilterer.getColumnIndex(
+                Attribute.speciesLatinName
+            );
+            const counts = groupIndices.map((indices) =>
+                indices.reduce((a, b) => (a + (dataSubset[b][speciesColIdx] == species) ? 1 : 0), 0)
+            );
+            return {
+                type: 'bar',
+                x: months,
+                y: counts,
+                marker: {
+                    color: Array(12).fill(this.config.traceColor[idx])
+                },
+                name: speciesMeta.formattedName(species)
+            };
+        });
+        const plotData = traces;
         const plotLayout = {
-            width: 290,
+            width: 400,
             height: 210,
             title: {
                 text: 'Bar Chart',
