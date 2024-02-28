1c86ae23516646983f78d331b0bf00183e3a0530
CGDogan <126820728+CGDogan@users.noreply.github.com>
2024-02-28 17:15:57 +0000

    remove comments

--- devfiles/src/classes/data/datautils/csvreader.test.ts
+++ devfiles/src/classes/data/datautils/csvreader.test.ts
@@ -1,41 +1,28 @@
 import { parseCSV } from './csvreader';
 
-let assert;
-if (!assert) {
-    assert = (x) => {
-        if (!x) {
-            console.error('Error found!');
-        }
-    };
-}
-
-/* eslint no-undef: off */
 test('valid CSV files', () => {
-    /*console.log*/ expect(parseCSV('a,b,c\ne,f,g', undefined));
-    /*console.log*/ expect(parseCSV('a,b,c\ne,f,g\n', undefined));
-    /*console.log*/ expect(parseCSV('a,b,c\n\ne,f,g\n\n', undefined));
-    /*console.log*/ expect(parseCSV('a,b,c\r\ne,f,g\r\n', undefined));
-    /*console.log*/ expect(parseCSV('a,b,c\r\ne,,g\r\n', undefined));
-    /*console.log*/ expect(parseCSV('a,b,c\r\n,,g\r\n', undefined));
-    /*console.log*/ expect(parseCSV('a,b,c\r\n,,\r\n', undefined));
-    /*console.log*/ expect(parseCSV('\n\n\r\n"a,b",b,c\r\nx,"y,t",z\r\n', undefined));
-    /*console.log*/ expect(parseCSV('"a,b",b,c\r\nx,"y\n,,t",z\r\n', undefined));
-    /*console.log*/ expect(parseCSV('a\ne', undefined));
-    /*console.log*/ expect(parseCSV(`a"a"\ne`, undefined));
-    /*console.log*/ expect(parseCSV(`"a","b"\ne,f`, undefined));
-    /*console.log*/ expect(parseCSV(`"a",a"b"\ne,f`, undefined));
-    /*console.log*/ expect(parseCSV(`,,,\na,b,c,d`, undefined));
-    /*console.log*/ expect(parseCSV(`,"a\n"`, undefined));
-    /*console.log*/ expect(parseCSV(`a,"a\n"\nx,"y\n"`, undefined));
-    /*console.log*/ expect(parseCSV(`a,"a,\nb"`, undefined));
-    /*console.log*/ expect(parseCSV(`a,"a\n,b"`, undefined));
-    /*console.log*/ expect(parseCSV(`a,"a,b"`, undefined));
-    /*console.log*/ expect(parseCSV(`a,b,c,d`, undefined));
-
-    /*expect(linkElement).toBeInTheDocument();*/
+    expect(parseCSV('a,b,c\ne,f,g', undefined));
+    expect(parseCSV('a,b,c\ne,f,g\n', undefined));
+    expect(parseCSV('a,b,c\n\ne,f,g\n\n', undefined));
+    expect(parseCSV('a,b,c\r\ne,f,g\r\n', undefined));
+    expect(parseCSV('a,b,c\r\ne,,g\r\n', undefined));
+    expect(parseCSV('a,b,c\r\n,,g\r\n', undefined));
+    expect(parseCSV('a,b,c\r\n,,\r\n', undefined));
+    expect(parseCSV('\n\n\r\n"a,b",b,c\r\nx,"y,t",z\r\n', undefined));
+    expect(parseCSV('"a,b",b,c\r\nx,"y\n,,t",z\r\n', undefined));
+    expect(parseCSV('a\ne', undefined));
+    expect(parseCSV(`a"a"\ne`, undefined));
+    expect(parseCSV(`"a","b"\ne,f`, undefined));
+    expect(parseCSV(`"a",a"b"\ne,f`, undefined));
+    expect(parseCSV(`,,,\na,b,c,d`, undefined));
+    expect(parseCSV(`,"a\n"`, undefined));
+    expect(parseCSV(`a,"a\n"\nx,"y\n"`, undefined));
+    expect(parseCSV(`a,"a,\nb"`, undefined));
+    expect(parseCSV(`a,"a\n,b"`, undefined));
+    expect(parseCSV(`a,"a,b"`, undefined));
+    expect(parseCSV(`a,b,c,d`, undefined));
 });
 
-/* eslint no-undef: off */
 test('invalid CSV files', () => {
     expect(() => parseCSV('a,b\nc,d,e,e,f', undefined)).toThrow();
     expect(() => parseCSV('a,b\nc', undefined)).toThrow();
