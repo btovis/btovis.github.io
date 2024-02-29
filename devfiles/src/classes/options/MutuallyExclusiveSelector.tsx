8fe2f1113a69235d242e4471e259c1a1f6031456
George Ogden <george-ogden@outlook.com>
2024-02-29 09:58:08 +0000

    implement mutually exclusive selector
    small changes to Selector.tsx to make this work

--- devfiles/src/classes/options/MutuallyExclusiveSelector.tsx
+++ devfiles/src/classes/options/MutuallyExclusiveSelector.tsx
@@ -1,14 +1,22 @@
+import Panel from '../Panel';
 import { Query } from '../query/Query';
 import InputOption from './InputOption';
+import Selector from './Selector';
 
-export default class MutuallyExclusiveSelector extends InputOption {
-    public render(): JSX.Element {
-        return <p>MutuallyExclusiveSelector: {this.name}</p>;
+export default class MutuallyExclusiveSelector extends Selector {
+    public constructor(
+        panel: Panel,
+        name: string,
+        choices: string[] | number,
+        default_?: string,
+        template: Selector = undefined
+    ) {
+        if (default_ === undefined) {
+            default_ = choices[0];
+        }
+        super(panel, name, choices, false, [default_], template);
     }
-    public callback(newValue: any): void {
-        throw new Error('Method not implemented.');
-    }
-    public query(): Query {
-        throw new Error('Method not implemented.');
+    protected inputType(): string {
+        return 'radio';
     }
 }
