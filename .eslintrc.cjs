4872fe60983755c72d8a8ae5e60ab558efb770c9
George Ogden <george-ogden@outlook.com>
2024-03-02 20:48:55 +0000

    error on unused variables

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,8 +14,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "warn",
-	"@typescript-eslint/no-explicit-any": "off"
+
+    "@typescript-eslint/no-explicit-any": "off"
   },
 }
