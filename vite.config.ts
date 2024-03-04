a4747049db6da51f93f2f6b344fd6d2b7af90cbc
George Ogden <george-ogden@outlook.com>
2024-03-04 14:32:34 +0000

    use plugin to copy favicon

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        viteStaticCopy({
+            targets: [
+                // relative path is from src
+                { from: './assets/favicon.ico', to: './' }
+            ]
+        })
+    ],
     root: 'devfiles',
     build: {
         outDir: '../dist',
